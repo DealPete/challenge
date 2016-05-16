ed4a714125e0b92f2db51c55fbffdc4e95d8f141
DealPete <pdeal@nl.rogers.com>
2016-05-15 22:41:23 -0230

    add more problems

--- billiard.hs
+++ billiard.hs
@@ -5,7 +5,7 @@ main = do
   if line == "0 0 0 0 0"
      then return ()
      else do let [a, b, s, m, n] = (map read).words $ line
-             let angle = (180/pi) * atan (n*b/(m*a)) :: Float
-             let speed = (1/s) * sqrt (m^2*b^2 + n^2*a^2) :: Float
-             printf "%f %f\n" angle speed
+             let angle = (180/pi) * atan (n*b/(m*a)) :: Double
+             let speed = (1/s) * sqrt (m^2*a^2 + n^2*b^2) :: Double
+             printf "%.2f %.2f\n" angle speed
              main
